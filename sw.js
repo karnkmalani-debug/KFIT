a9c105f411499b5a5137080bfe8d92fcf6c7a4b8
karnkmalani-debug <karnkmalani@gmail.com>
2026-09-02 13:41:53 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const V = "kfit-v1";
+self.addEventListener("install", e => { self.skipWaiting(); });
+self.addEventListener("activate", e => e.waitUntil(
+  caches.keys().then(ks => Promise.all(ks.filter(k => k !== V).map(k => caches.delete(k)))).then(() => self.clients.claim())
+));
+self.addEventListener("fetch", e => e.respondWith(
+  fetch(e.request).then(res => {
+    const clone = res.clone();
+    caches.open(V).then(c => c.put(e.request, clone));
+    return res;
+  }).catch(() => caches.match(e.request))
+));
